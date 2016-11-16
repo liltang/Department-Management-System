06599cd3110b1b71636da9402778f3adfb8f661c
wintersen <black95@me.com>
2016-11-15 19:07:26 -0500

    student header

---
+++ student.h
@@ -0,0 +1,13 @@
+//=================================================================
+//	STUDENT.H
+//	ERIKA WINTERS  COP 3331
+//=================================================================
+#include <string>
+
+class Student : public Person
+{
+  private:
+    string studentType
+  public:
+    Student(string name, string bDay, int uid, char gender, string studentType); //constructor
+}
